ad9f5c9afa9b8d01bfc914a9e36c4fc827731ec6
fuzisan <toshichanapp@gmail.com>
2018-03-26 14:19:06 +0000

    migration reset

--- db/migrate/20180326133012_contacts.rb
+++
@@ -1,9 +0,0 @@
-class Contacts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :contacts do |t|
-      t.string :name
-      t.string :email
-      t.text :content
-    end
-  end
-end

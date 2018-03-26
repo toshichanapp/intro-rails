58f8bdb728379e77861809a9cb0b577548592658
fuzisan <toshichanapp@gmail.com>
2018-03-26 14:19:34 +0000

    new create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,18 @@
 class ContactsController < ApplicationController
+    
+    def new
+        @contact = Contact.new
+    end
+    
+    def create
+        @contact = Contact.new(contact_params)
+        @contact.save!
+        redirect_to new_contact_url
+    end
+    
+    private
+    
+    def contact_params
+        params.require(:contact).permit(:neme, :email, :content)
+    end
 end

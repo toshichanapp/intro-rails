57310b9e3ab3a831674e7960da0937e83799028f
fuzisan <toshichanapp@gmail.com>
2018-03-27 11:55:08 +0000

    エラーメッセージ表示

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,23 @@
 class ContactsController < ApplicationController
-    
+
     def new
         @contact = Contact.new
     end
-    
+
     def create
         @contact = Contact.new(contact_params)
-        @contact.save!
-        redirect_to new_contact_url
+        if @contact.save
+            redirect_to new_contact_url
+        else
+
+            render :new
+        end
+
     end
-    
+
     private
-    
+
     def contact_params
-        params.require(:contact).permit(:neme, :email, :content)
+        params.require(:contact).permit(:name, :email, :content)
     end
 end

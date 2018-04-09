9680afa00b2cadedcf62306fa2853fe860cf3fb2
fuzisan <toshichanapp@gmail.com>
2018-04-09 14:55:19 +0000

    userとblogsのリレーション追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
+
+    belongs_to :user
+
+
     validates :title, presence:true
     validates :content, presence:true, length: { in: 1..140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :blogs
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   validates :name,  presence: true, length: { maximum: 30 }

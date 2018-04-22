ec549ec5a2e5626faeb0cda4953ca6a19ea893fe
fuzisan <toshichanapp@gmail.com>
2018-04-22 05:08:25 +0000

    action mailer hack

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -4,4 +4,12 @@ class BlogMailer < ApplicationMailer
     @blog = blog
     mail to:@blog.user.email, subject:"#{@blog.title}が作成されました"
   end
+
+  def deliver_later!
+    deliver_now!
+  end
+
+  def deliver_late
+    deliver_now
+  end
 end

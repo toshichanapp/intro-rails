ffe037fd12bdaca9150e1148772751ddaf82d9d6
fuzisan <toshichanapp@gmail.com>
2018-05-15 04:35:28 +0000

    ユーザフォロー機能追加 モデル側

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+  belongs_to :followed, class_name: "User"
+  belongs_to :follower, class_name: "User"
+
+  # 同じテーブルから複数のアソシエーションを定義する時
+  # アソシエーション名を変えることで管理する
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
 
   has_many :blogs
   has_many :favorites
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  # class_name: 'Relationship'
+  # Railsのアソシエーションは何もオプションを書かないと、
+  # アソシエーション名と同じ名前のテーブルをさがす。
+  # テーブルが存在しないアソシエーションを構築する時は、
+  # テーブルに紐づくモデル名（Relationship） をclass_nameオプションとして記載
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
